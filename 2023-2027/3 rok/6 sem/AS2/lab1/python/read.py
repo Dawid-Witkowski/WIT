ebffb39b4521ef517e39250b08a94d8605ce10a7
Dawid Witkowski <dawidwitkowski.dev@gmail.com>
2025-04-27 10:39:29 +0200

    java kolos 1

--- 2023-2027/3 rok/6 sem/AS2/lab1/python/read.py
+++
@@ -1,9 +0,0 @@
-# author: @y3snt
-
-def read_file():
-    with open("Selection_Insert_Bubble_0001.csv") as f:
-        parsed_file = f.read().split('\n')[:-1]
-        array = [int(x) for x in parsed_file]
-        print(array)
-
-    return array
